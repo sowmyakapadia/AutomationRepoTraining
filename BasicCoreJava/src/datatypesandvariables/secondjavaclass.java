2877790f3dc23604e495be6b934b858140ed9058
Sowmya <kapadiasowmya@gmail.com>
2019-09-29 12:09:35 +0530

    This is my second test

---
+++ BasicCoreJava/src/datatypesandvariables/secondjavaclass.java
@@ -0,0 +1,12 @@
+package datatypesandvariables;
+
+public class secondjavaclass {
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is a test message");
+		// TODO Auto-generated method stub
+
+	}
+
+}
