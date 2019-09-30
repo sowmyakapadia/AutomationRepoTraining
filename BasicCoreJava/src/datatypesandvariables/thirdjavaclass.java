af679cd71a79fd885d517c1dcb1e54ecd5823c0e
Sowmya <kapadiasowmya@gmail.com>
2019-09-30 13:13:40 +0530

    Test code for error message

---
+++ BasicCoreJava/src/datatypesandvariables/thirdjavaclass.java
@@ -0,0 +1,12 @@
+package datatypesandvariables;
+
+public class thirdjavaclass {
+
+	public static void main(String[] args)
+	{
+		System.err.println("Password incorrect");
+		// TODO Auto-generated method stub
+
+	}
+
+}
