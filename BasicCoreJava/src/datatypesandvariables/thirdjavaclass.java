643c48b984d6313940ea4b4b53557eb943b47f86
Sowmya <kapadiasowmya@gmail.com>
2019-10-02 11:19:03 +0530

    Updated Error message

--- BasicCoreJava/src/datatypesandvariables/thirdjavaclass.java
+++ BasicCoreJava/src/datatypesandvariables/thirdjavaclass.java
@@ -4,7 +4,7 @@ public class thirdjavaclass {
 
 	public static void main(String[] args)
 	{
-		System.err.println("Password incorrect");
+		System.err.println("This Password is incorrect");
 		// TODO Auto-generated method stub
 
 	}
