ea6d90343c7e3a3a42f8ec9f4534d389da2e7097
Sowmya <kapadiasowmya@gmail.com>
2019-10-02 11:23:36 +0530

    Changes made in message

--- BasicCoreJava/src/datatypesandvariables/secondjavaclass.java
+++ BasicCoreJava/src/datatypesandvariables/secondjavaclass.java
@@ -4,7 +4,7 @@ public class secondjavaclass {
 
 	public static void main(String[] args)
 	{
-		System.out.println("This is a test message");
+		System.out.println("This is a new test message");
 		// TODO Auto-generated method stub
 
 	}
