ce25e9be68e7ad984f7427f16dfcf57adeca14d9
Sowmya <kapadiasowmya@gmail.com>
2019-10-03 18:07:52 +0530

    Testpackage1 new class

---
+++ TestJava1/src/datatypesandvariables/TestPackage1.java
@@ -0,0 +1,12 @@
+package datatypesandvariables;
+
+public class TestPackage1 {
+
+	public static void main(String[] args)
+	{
+		System.err.println("The login credentials entered are incorrect");
+		// TODO Auto-generated method stub
+
+	}
+
+}
