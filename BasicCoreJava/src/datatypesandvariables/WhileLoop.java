27962ceadf600be70475cc6427f87202966b3511
Sowmya <kapadiasowmya@gmail.com>
2019-10-06 08:48:54 +0530

    Committing second set of java files

---
+++ BasicCoreJava/src/datatypesandvariables/WhileLoop.java
@@ -0,0 +1,22 @@
+package datatypesandvariables;
+
+public class WhileLoop {
+
+	public static void main(String[] args) 
+	{
+		//Initialization
+		
+		int i=1;
+		
+		while(i<10) //Condition
+		
+		{
+			System.out.println(i);
+		i++;//Increment / Decrement
+		
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
