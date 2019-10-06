d7f86963f2b8d150b50c82c8d5d3feb55f8cd535
Sowmya <kapadiasowmya@gmail.com>
2019-10-06 08:57:19 +0530

    Logical operator code committed

---
+++ BasicCoreJava/src/datatypesandvariables/LogicalOperator1.java
@@ -0,0 +1,25 @@
+package datatypesandvariables;
+
+public class LogicalOperator1 {
+
+	public static void main(String[] args) 
+	{
+		int age=31;
+		int salary=1000;
+		
+		//Using Logical operator (And)
+		
+		if(age>=18 && age<=65 && salary>=3000)
+		{ System.out.println("You are eligible");
+		}else {
+			System.out.println("Your are not eligible");
+		}
+		
+			
+	}
+}
+		// TODO Auto-generated method stub
+
+	
+
+
