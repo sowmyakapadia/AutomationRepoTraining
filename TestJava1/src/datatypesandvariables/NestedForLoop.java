1f578a5e3d853afe7976c1c125cce27d382b9abf
Sowmya <kapadiasowmya@gmail.com>
2019-10-14 19:28:40 +0530

    Nested for loop done at home

---
+++ TestJava1/src/datatypesandvariables/NestedForLoop.java
@@ -0,0 +1,17 @@
+package datatypesandvariables;
+
+public class NestedForLoop {
+	public static void main(String[] args) {
+		
+		//Initialization of datatype and variables
+		for (int r = 1; r <=8; r++) 
+		{ for (int c = 1; c <=8; c++) 
+		{System.out.println("Row no "+r+" and coloumn no "+c );
+		
+		}
+			System.out.println();
+		}		
+		
+	}
+
+}
