48dbc8b4cf631ed5e98c1d240335519edd37574e
Sowmya <kapadiasowmya@gmail.com>
2019-10-28 11:58:54 +0530

    Exception Handling

---
+++ TestJava1/src/TestExceptionHandling/TestException1.java
@@ -0,0 +1,18 @@
+package TestExceptionHandling;
+
+import TestOopsAbstraction.TestDevDept;
+
+public class TestException1 {
+static TestDevDept dept;
+ 
+public static void main(String[] args) {
+	try {System.out.println(10/0);
+		
+	} catch (Exception e) {
+		
+	}
+}
+ 
+
+
+}
