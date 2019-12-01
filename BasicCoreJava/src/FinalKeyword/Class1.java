10b0632f7f2d6ec5283409bd92e67d13b95ea8dc
Sowmya <kapadiasowmya@gmail.com>
2019-12-01 18:25:53 +0530

    Final Class

---
+++ BasicCoreJava/src/FinalKeyword/Class1.java
@@ -0,0 +1,13 @@
+package FinalKeyword;
+
+public class Class1 extends Class2 {
+//final keyword does not allow reassignment of varaiable name
+	public final static String name="Sowmya";
+	
+	public static void main(String[] args) {
+		name="Ashu";
+		
+	}
+	
+	public void test(){}
+}
---
+++ BasicCoreJava/src/FinalKeyword/Class2.java
@@ -0,0 +1,11 @@
+package FinalKeyword;
+//Does not allow extension of class 2. Check Class1 for error
+//public final class Class2
+public class Class2 {
+	
+//final keyword on method disallows its calling even when class is extended. Check Class1	
+	public final void test(){
+		
+	}
+
+}
