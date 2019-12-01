86b3432121ee4ed33f0d2400292abc72f9f81435
Sowmya <kapadiasowmya@gmail.com>
2019-12-01 18:25:26 +0530

    String class

--- BasicCoreJava/src/stringclass/Assignment.java
+++ BasicCoreJava/src/stringclass/Assignment.java
@@ -2,12 +2,14 @@ package stringclass;
 
 public class Assignment {
 	String name="anand";
+	char str;
+	
 	
 	public void main(String[] args) {
 //find the number of times n is repeated in a string		
 //		System.out.println(name.length()-name.replace("n", "")).lenght());
 		System.out.println(name.length()-name.replace("n", "").length());
-		
-	}
+	
+		}
 
 }
